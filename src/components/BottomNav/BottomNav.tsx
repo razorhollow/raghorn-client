6cb138b2e523d0739e3bd42c6ec061792c539c29
razorhollow <rob@razorhollow.com>
2023-04-13 14:29:18 -0400

    clear type errors

--- src/components/BottomNav/BottomNav.tsx
+++ src/components/BottomNav/BottomNav.tsx
@@ -15,6 +15,7 @@ import FormControl from '@mui/material/FormControl';
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import Select from '@mui/material/Select';
+import { SelectChangeEvent } from '@mui/material/Select';
 import * as postService from '../../services/postService'
 
 //types
@@ -50,7 +51,7 @@ export default function BottomNav(props: BottomNavProps) {
     setOpen(false);
   };
 
-  const handleCategoryChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+  const handleCategoryChange = (event: SelectChangeEvent<string>) => {
     setSelectedCategory(event.target.value);
     const categoryId = parseInt(event.target.value)
     setFormData({ ...formData, categoryId: categoryId })
